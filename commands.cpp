7961abcefc38149f21af1660e0af6d0be6ee634c
Vaughn Gugger <vjgtigers@gmail.com>
2024-05-03 16:05:34 -0400

    prevent crash on dir DNE

--- commands.cpp
+++ commands.cpp
@@ -5,18 +5,19 @@
 #include "commands.h"
 
 #include <string>
-
+#include <iostream>
+#include <sys/stat.h>
 #include "miscFunctions.h"
 
 
 void changeDir(const std::string& command) {
         std::string subCommand = command.substr(command.find(' ') +1);
-        debugOutput(subCommand, -10);
-
-        debugOutput(std::to_string(subCommand.back()) + "ansoteuh", -11);
+        struct stat sb;
+        if (stat(subCommand.c_str(), &sb) != 0) {
+                return;
+        }
         if (subCommand.back() != '\\') {
                 subCommand += '\\';
-
         }
         path_dir = subCommand;
         backOneDir(fileInformation, path_dir);

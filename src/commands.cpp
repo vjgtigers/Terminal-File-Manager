68efc3710a0342315cb65a1c047b76d2443209b7
Vaughn Gugger <vjgtigers@gmail.com>
2024-05-06 12:48:30 -0400

    restarted CLion - tab amount is fixed now

--- src/commands.cpp
+++ src/commands.cpp
@@ -19,61 +19,62 @@
 //update dir
 
 
-void displayError(const std::string& message) {
-        xy wd = detectSize();
-        setCursorPosition(0, wd.y-1);
-        std::cout << message;
+void displayError(const std::string &message) {
+    xy wd = detectSize();
+    setCursorPosition(0, wd.y - 1);
+    std::cout << message;
 }
 
-void changeDir(const std::string& command) {
-        std::string subCommand = command.substr(command.find(' ') +1);
-        struct stat sb;
+void changeDir(const std::string &command) {
+    std::string subCommand = command.substr(command.find(' ') + 1);
+    struct stat sb;
 
-        if (subCommand[1] == ':') {
-                if (subCommand.back() != '\\') {
-                        subCommand += '\\';
-                }
-                path_dir = subCommand;
+    if (subCommand[1] == ':') {
+        if (subCommand.back() != '\\') {
+            subCommand += '\\';
         }
-        else if (subCommand.substr(0,2) == "..") {
-                char full[_MAX_PATH];
-                if (_fullpath(full, (path_dir+"\\"+subCommand).c_str(), _MAX_PATH) != NULL) {
-                        path_dir = full;
-                } else {
-                        displayError("Change Dir Failed");
-                }
+        path_dir = subCommand;
+    } else if (subCommand.substr(0, 2) == "..") {
+        char full[_MAX_PATH];
+        if (_fullpath(full, (path_dir + "\\" + subCommand).c_str(), _MAX_PATH) != NULL) {
+            path_dir = full;
         } else {
-                path_dir = path_dir + "\\" + subCommand + "\\";
+            displayError("Change Dir Failed");
         }
-         if (stat(subCommand.c_str(), &sb) != 0) {
-                displayError("Invalid directory");
-                return;
-         }
+    } else {
+        path_dir = path_dir + "\\" + subCommand + "\\";
+    }
+    if (stat(subCommand.c_str(), &sb) != 0) {
+        displayError("Invalid directory");
+        return;
+    }
 
-        backOneDir(fileInformation, path_dir);
-
+    backOneDir(fileInformation, path_dir);
 }
 
 
-void displayHelp(const std::string& command) {
+void displayHelp(const std::string &command) {
     std::string subCommand = command.substr(command.find(' ') + 1);
     debugOutput(subCommand + " aouthn", -14);
-    std::ifstream openFile ("./helpInfo/" + subCommand + ".txt");
+    std::ifstream openFile("./helpInfo/" + subCommand + ".txt");
     if (openFile.is_open()) {
-            debugOutput(subCommand + " 11111", -14);
-            std::string fileData;
-            clearScreen();
-            setCursorPosition(0,0);
-            while (openFile) {
-                    fileData = openFile.get();
-                    std::cout << fileData;
-            }
-    } else {displayError("Command Does Not exist"); return;}
-        while(true) {
-                const int key = key_press();
-                if (key == 'q') {
-                        maintainStateRefresh(fileInformation);
-                        break;
-                }
+        debugOutput(subCommand + " 11111", -14);
+        std::string fileData;
+        clearScreen();
+        setCursorPosition(0, 0);
+        while (openFile) {
+            fileData = openFile.get();
+            std::cout << fileData;
         }
+    } else {
+        displayError("Command Does Not exist");
+        return;
+    }
+    while (true) {
+        const int key = key_press();
+        if (key == 'q') {
+            maintainStateRefresh(fileInformation);
+            break;
+        }
+    }
 }

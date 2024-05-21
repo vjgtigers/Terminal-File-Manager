bf205935899c38db4e3cde53b2777f245241fd7e
Vaughn Gugger <vjgtigers@gmail.com>
2024-05-21 14:56:37 -0400

    temp: live search files added with ideas

---
+++ src/liveSearch.cpp
@@ -0,0 +1,14 @@
+//
+// Created by vjgti on 5/21/2024.
+//
+
+#include "liveSearch.h"
+
+//clear screen
+//while true loop
+//section to display what type of search is working. ie regular regex etc
+//search
+//list of valid files
+    //can i scroll through these?
+//amount of files returned
+//recursive search?
---
+++ src/liveSearch.h
@@ -0,0 +1,8 @@
+//
+// Created by vjgti on 5/21/2024.
+//
+
+#ifndef LIVESEARCH_H
+#define LIVESEARCH_H
+
+#endif //LIVESEARCH_H
